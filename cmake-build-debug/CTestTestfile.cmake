c7aa3bfb4971d25b5b6c42cfaded8b17d7922ec2
Sidsky <Siddharthmehrotra1234@gmail.com>
2023-11-24 10:20:10 +0800

    Remove IDE files

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/siddharthmehrotra/Library/Mobile Documents/com~apple~CloudDocs/Scientific C++/LHFT
-# Build directory: /Users/siddharthmehrotra/Library/Mobile Documents/com~apple~CloudDocs/Scientific C++/LHFT/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib/googletest")
-subdirs("test")
